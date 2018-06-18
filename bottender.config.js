38278a410d3a6a3c21a15ba65d459f761ad9fdd0
aw0lf <adrianna,tycner@yahoo.com>
2018-06-18 14:39:55 +0200

    start

---
+++ bottender.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  messenger: {
+    accessToken: 'EAAYDbIsH7REBALmriUTAHpiiXoxgQNeCwLccYlbPS1d0GzdSdG5rZAB18ZAhZATtR3hF8fSEkw1Edqu7mOsJDlMqPMeRicIUQsEB8PU7zMCzUOZCO9GwR8kBl6GlEVk3ZBinK1D8vxGdHOhUvPpKWZAq8RHTh9FFQx5LIeIkzPBAZDZD',
+    verifyToken: 'imadethisup',
+    appId: '1692614584167697',
+    appSecret: '8eaea7817c33f842cf94667162338188'
+  }
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+const { MessengerBot, MongoSessionStore } = require('bottender');
+const { createServer } = require('bottender/express');
+
+const config = require('./bottender.config.js').messenger;
+
+const bot = new MessengerBot({
+  accessToken: config.accessToken,
+  appSecret: config.appSecret,
+  sessionStore: new MongoSessionStore('mongodb://localhost:27017/'),
+});
+
+bot.onEvent(async context => {
+  await context.sendText('Hello World');
+});
+
+const server = createServer(bot, { verifyToken: config.verifyToken });
+
+server.listen(5000, () => {
+  console.log('server is running on 5000 port...');
+});
